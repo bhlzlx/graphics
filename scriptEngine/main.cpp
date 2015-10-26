649451a795c189e9a2a7711da040495af0fd8b3a
bhlzlx <bhlzlx@gmail.com>
2015-10-26 18:16:17 +0800

    添加 scriptEngine 工程

---
+++ scriptEngine/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <lua/lua.h>
+#include <lua/luaconf.h>
+#include <lua/lualib.h>
+#include <lua/lauxlib.h>
+
+#include <LuaBridge/LuaBridge.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
+}
