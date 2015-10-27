920bfe02d6e4144f4ff4f5c9bfa16768f7ae4072
bhlzlx <bhlzlx@gmail.com>
2015-10-27 18:16:50 +0800

    更新测试脚本

--- scriptEngine/script.lua
+++ scriptEngine/script.lua
@@ -1,7 +1,37 @@
+function script_VoidFunction()
+	print "VoidFunction"
+end
 
-function trace()
-	local obj = Object();
-	obj.m_type = 1;
-	obj:trace();
-	print("trace from script!");
+function script_GetIntValue()
+	return 100;
+end
+function script_GetFloatValue()
+	return 1.6666;
+end
+function script_GetStringValue()
+	return "Hello,World!"
+end
+
+function script_VoidParam1( value )
+	print("passed value is : "..value);
+end
+
+function script_Param1( value )
+	return value + 1;
+end
+
+function script_MemBufferRet()
+	local memBuffer = ow.CreateMemBuffer(1024);
+	memBuffer:Release();
+	memBuffer:Resize(2046);
+	--[[
+	local str = "Hello,World!";
+	local len = 13;
+	memBuffer:Write(str, 13);
+	return memBuffer;
+	]]
+end
+
+function script_MemBufferRelease( memBuffer )
+	memBuffer:Release();
 end
